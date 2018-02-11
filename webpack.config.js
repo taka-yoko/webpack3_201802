a61423cb52c849c17b1e49678d1bc3574c603c69
横山貴芳 <t-yoko@t-yoko.local>
2018-02-11 12:50:22 +0900

    add sourceMap and minify conf

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: './src/js/main.js',
   output: {
     path: `${__dirname}/build`,
     filename: 'bundle.js'
-  }
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    })
+  ]
 };
